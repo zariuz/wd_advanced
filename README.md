3990557a4b4413d9448b59542fe57fce293691f9
zariuz <nechetov@hotmail.com>
2017-12-14 14:53:17 +0300

    add gulp and webpack

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Nechetov Evgeniy
+Loft School, webdev advanced 12/2017
+
+npm 5
+gulp-cli
+
+Install:
+1.Git clone
+2.npm i
---
+++ gulpfile.js
@@ -0,0 +1,130 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
+const autoprefixer = require('gulp-autoprefixer');
+const svgSprite = require('gulp-svg-sprites');
+const cheerio = require('gulp-cheerio');
+
+
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug'
+    },
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'build/assets/styles/'
+    },    
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'build/assets/scripts/'
+    }
+}
+
+// pug
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(paths.root));
+}
+
+// scss
+function styles() {
+    return gulp.src('./src/styles/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+// clean
+function clean() {
+    return del(paths.root);
+}
+
+// webpack
+function scripts() {
+    return gulp.src('src/scripts/app.js')
+        .pipe(gulpWebpack(webpackConfig, webpack)) 
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// галповский вотчер
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+// локальный сервер + livereload (встроенный)
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+// svg
+function svg() {
+    return gulp.src(paths.svg.src)
+        // remove attribute
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        // svg sprite
+        .pipe(svgSprite({
+            mode: "symbols",
+            selector: "icon-%f",
+            svg: {
+                symbols: "sprite.svg"
+            },
+            preview: false
+            }
+        ))
+        .pipe(gulp.dest(paths.svg.dest));
+}
+
+// переносим картинки
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+// переносим шрифты
+function fonts() {
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
+}
+
+exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+exports.images = images;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images, scripts),
+    gulp.parallel(watch, server)
+));
