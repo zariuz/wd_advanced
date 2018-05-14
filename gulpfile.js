5214ad950a969a2c62ad80c8b7c82a4fd1072f64
zariuz <nechetov@hotmail.com>
2018-05-14 17:46:38 +0300

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,55 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
+const gulp          = require('gulp');
+const pug           = require('gulp-pug');
+const sass          = require('gulp-sass');
+const rename        = require('gulp-rename');
 
-const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-const browserSync = require('browser-sync').create();
+const sourcemaps    = require('gulp-sourcemaps');
+const del           = require('del');
+const browserSync   = require('browser-sync').create();
 
-const gulpWebpack = require('gulp-webpack');
-const webpack = require('webpack');
+const gulpWebpack   = require('gulp-webpack');
+const webpack       = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 
-const autoprefixer = require('gulp-autoprefixer');
-const svgSprite = require('gulp-svg-sprites');
-const svgmin = require('gulp-svgmin');
-const cheerio = require('gulp-cheerio');
+const autoprefixer  = require('gulp-autoprefixer');
+const svgSprite     = require('gulp-svg-sprites');
+const svgmin        = require('gulp-svgmin');
+const cheerio       = require('gulp-cheerio');
 
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const imagemin = require('gulp-imagemin');
-const normalize = require('node-normalize-scss');
-const replace = require('gulp-replace');
+const plumber       = require('gulp-plumber');
+const notify        = require('gulp-notify');
+const imagemin      = require('gulp-imagemin');
+const normalize     = require('node-normalize-scss');
+const replace       = require('gulp-replace');
 
 
 const paths = {
-  root: './build',
+    root: './build',
 
-  pug: {
-    pages: 'src/pug/pages/*.pug',
-    src: 'src/pug/**/*.pug',
-    dest: 'build/',
+    pug: {
+        pages: 'src/pug/pages/*.pug',
+        src: 'src/pug/**/*.pug',
+        dest: 'build/',
   },
-  styles: {
-    src: 'src/scss/**/*.scss',
-    dest: 'build/css/',
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'build/css/',
   },    
-  images: {
-    src: 'src/img/{bg,content,icons}/*',
-    dest: 'build/img/',
+    images: {
+        src: 'src/img/{bg,content,icons}/*',
+        dest: 'build/img/',
   },
-  scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'build/js/',
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'build/js/',
   },
-  fonts: {
-    src: 'src/fonts/**/*.*',
-    dest: 'build/fonts/',
+    fonts: {
+        src: 'src/fonts/**/*.*',
+        dest: 'build/fonts/',
   },
-  svg: {
-    src: 'src/img/icons/*.svg',
-    dest: 'build/img',
+    svg: {
+        src: 'src/img/icons/*.svg',
+        dest: 'build/img',
   }
 }
 
@@ -79,31 +79,31 @@ function scss() {
 
 // clean
 function clean() {
-  return del(paths.root);
+    return del(paths.root);
 }
 
 // webpack
 function scripts() {
-  return gulp.src('src/js/main.js')
-    .pipe(gulpWebpack(webpackConfig, webpack)) 
-    .pipe(gulp.dest(paths.scripts.dest));
+    return gulp.src('src/js/main.js')
+        .pipe(gulpWebpack(webpackConfig, webpack)) 
+        .pipe(gulp.dest(paths.scripts.dest));
 }
 
 // галповский вотчер
 function watch() {
 	gulp.watch(paths.pug.src, templates);    
 	gulp.watch(paths.styles.src, scss);
-  gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.images.src, imgMin);    
-  gulp.watch(paths.fonts.src, fonts);        
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.images.src, imgMin);    
+    gulp.watch(paths.fonts.src, fonts);        
 }
 
 // локальный сервер + livereload (встроенный)
 function server() {
-  browserSync.init({
-    server: paths.root
-  });
-  browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
 // img
@@ -121,26 +121,26 @@ function sprites() {
   return gulp.src('src/img/svg/*.svg')
     .pipe(plumber())    
     .pipe(svgmin({
-      js2svg: {
-        pretty: true
-      }
+        js2svg: {
+            pretty: true
+        }
     }))
     .pipe(cheerio({
-      run: function ($) {
-        $('[fill]').removeAttr('fill');
-        $('[style]').removeAttr('style');
-      },
-      parserOptions: { xmlMode: false },
+        run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[style]').removeAttr('style');
+        },
+        parserOptions: { xmlMode: false },
     }))
     .pipe(replace('&gt;', '>'))
     .pipe(svgSprite({ 
-      mode: 'symbols',
-      preview: false,
+        mode: 'symbols',
+        preview: false,
     }))
     .pipe(gulp.dest('src/img/'))        
     .pipe(rename({ 
-      basename: 'sprite',
-      suffix: '.min',
+        basename: 'sprite',
+        suffix: '.min',
     }))
 //      .pipe(notify('Create sprite svg success'))
     .pipe(plumber.stop())
@@ -149,14 +149,14 @@ function sprites() {
 
 // переносим картинки
 function images() {
-  return gulp.src(paths.img.src)
+    return gulp.src(paths.img.src)
     .pipe(gulp.dest(paths.img.dest));
 }
 
 // переносим шрифты
 function fonts() {
-  return gulp.src(paths.fonts.src)
-//    .pipe(notify('Fonts success'))
+    return gulp.src(paths.fonts.src)
+//      .pipe(notify('Fonts success'))
     .pipe(gulp.dest(paths.fonts.dest))
 }
 
